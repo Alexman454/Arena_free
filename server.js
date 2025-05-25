a831b8e408ee638b0e8ed9938c57a911e766dfac
Yan4ikNya <Trakowakcat100.galkin@yandex.ru>
2025-05-25 22:12:36 +0300

    Добавление отображения игроков

--- server.js
+++ server.js
@@ -16,23 +16,42 @@ app.use(cors({
     origin: "*", // Укажи точный адрес, с которого открываешь index.html
     methods: ["GET", "POST"]
 }));
-// Обработка подключения клиента
+const players = {}; // Храним всех игроков по socket.id
+
 io.on("connection", (socket) => {
     console.log("New client connected:", socket.id);
 
-    // Пример получения события "move" от клиента
+    // Создаём нового игрока
+    const newPlayer = {
+        x: Math.random() * 700,
+        y: Math.random() * 500,
+        color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+    };
+    players[socket.id] = newPlayer;
+
+    // Отправляем клиенту его ID и список всех игроков
+    socket.emit("init", { id: socket.id, players });
+
+    // Уведомляем других клиентов о новом игроке
+    socket.broadcast.emit("playerJoined", { id: socket.id, player: newPlayer });
+
     socket.on("move", (data) => {
-        console.log("Happend move:", data);
-        // Рассылаем данные всем подключенным клиентам
-        io.emit("playerMoved", data);
+        if (players[socket.id]) {
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+
+            // Отправляем другим обновление позиции
+            socket.broadcast.emit("update", { id: socket.id, x: data.x, y: data.y });
+        }
     });
 
     socket.on("disconnect", () => {
         console.log("Client disconnected:", socket.id);
+        delete players[socket.id];
+        socket.broadcast.emit("playerLeft", socket.id);
     });
 });
 
-// Запуск сервера
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server port: ${PORT}`);
