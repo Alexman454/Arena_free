95469dda4f1e1705d45a0b65ab46c1592590e5b1
Yan4ikNya <Trakowakcat100.galkin@yandex.ru>
2025-05-25 23:11:37 +0300

    Доработка ошибок связанных со стрельбой и повышение тикрейта игры

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use(cors({
     methods: ["GET", "POST"]
 }));
 const players = {}; // Храним всех игроков по socket.id
-
+const bullets = [];
+function killPlayer(id) {
+    console.log(`Player ${id} was killed`);
+    delete players[id];
+    io.emit("playerLeft", id);
+}
 io.on("connection", (socket) => {
     console.log("New client connected:", socket.id);
 
@@ -25,7 +30,8 @@ io.on("connection", (socket) => {
     const newPlayer = {
         x: Math.random() * 700,
         y: Math.random() * 500,
-        color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+        color: "#" + Math.floor(Math.random() * 16777215).toString(16),
+        hp: 100 
     };
     players[socket.id] = newPlayer;
 
@@ -40,25 +46,29 @@ io.on("connection", (socket) => {
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
 
-            // Отправляем другим обновление позиции
-            socket.broadcast.emit("update", { id: socket.id, x: data.x, y: data.y });
+            // Рассылаем позицию и здоровье другим
+            socket.broadcast.emit("update", {
+                id: socket.id,
+                x: data.x,
+                y: data.y,
+                hp: players[socket.id].hp
+            });
         }
     });
 
     socket.on("disconnect", () => {
-        console.log("Client disconnected:", socket.id);
         delete players[socket.id];
         socket.broadcast.emit("playerLeft", socket.id);
     });
     socket.on("shoot", (data) => {
+        console.log(`Shoot event from ${socket.id}:`, data);
         const bullet = {
             x: data.x,
             y: data.y,
             dir: data.dir,
             owner: socket.id
         };
-
-        // рассылаем всем игрокам
+        bullets.push(bullet);
         io.emit("bulletFired", bullet);
     });
 
@@ -68,3 +78,48 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server port: ${PORT}`);
 });
+setInterval(() => {
+    for (let i = bullets.length - 1; i >= 0; i--) {
+        const b = bullets[i];
+
+        b.x += b.dir.x * 8;
+        b.y += b.dir.y * 8;
+
+        // Удаляем пулю, если она вышла за пределы игрового поля
+        if (b.x < 0 || b.x > 800 || b.y < 0 || b.y > 600) {
+            bullets.splice(i, 1);
+            continue;
+        }
+
+        // Проверяем попадание в игроков
+        for (const id in players) {
+            if (id === b.owner) continue;
+
+            const p = players[id];
+            if (
+                b.x > p.x &&
+                b.x < p.x + 30 &&
+                b.y > p.y &&
+                b.y < p.y + 30
+            ) {
+                // Уменьшаем здоровье
+                p.hp -= 25;
+
+                // Удаляем пулю
+                bullets.splice(i, 1);
+
+                // Обновляем всех клиентов по игроку
+                io.emit("update", {
+                    id,
+                    x: p.x,
+                    y: p.y,
+                    hp: p.hp,
+                });
+
+                if (p.hp <= 0) {
+                    killPlayer(id);
+                }
+            }
+        }
+    }
+}, 1000 / 120);
